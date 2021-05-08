9aed794de4a7c76edf93def43e3b14b6766cb955
Diana2410-start <yakusheva2410@yandex.ru>
2021-05-08 17:43:21 +0300

    вывод

--- conclusion.py
+++ conclusion.py
@@ -21,19 +21,19 @@ text_2 = font.render("Eccentricity e =" + q[1], True, green, blue)
 text_2Rect = text_2.get_rect()
 text_2Rect.center = (X // 3, Y // 3)
 
-text_3 = font.render(print_kepler(q), True, green, blue)
+text_3 = font.render("Inclination i =" + q[2] / np.pi * 180, True, green, blue)
 text_3Rect = text_3.get_rect()
 text_3Rect.center = (X // 3, Y // 2)
 
-text_4 = font.render(print_kepler(q), True, green, blue)
+text_4 = font.render("Longitude of the ascending node O =" + q[3] / np.pi * 180, True, green, blue)
 text_4Rect = text_4.get_rect()
 text_4Rect.center = (X // 3, 2 * Y // 3)
 
-text_5 = font.render(print_kepler(q), True, green, blue)
+text_5 = font.render("Argument of periapsis o =" + q[4] / np.pi * 180, True, green, blue)
 text_5Rect = text_5.get_rect()
 text_5Rect.center = (X // 3, 5 * Y // 6)
 
-text_6 = font.render(print_kepler(q), True, green, blue)
+text_6 = font.render("True anomaly th =" + q[5] / np.pi * 180, True, green, blue)
 text_6Rect = text_6.get_rect()
 text_6Rect.center = (X // 3, Y)
 
