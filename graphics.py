70a003a76a8731d86e62297853a36e54229fa374
Karim Vafin <karim.vafin@mail.ru>
2021-05-09 19:32:56 +0300

    refactored animation by OOP, add loading window

--- graphics.py
+++ graphics.py
@@ -2,67 +2,197 @@ import pygame
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from trasfomation import to_polar, to_kepler
-FPS = 60
+import numpy as np
+from abc import abstractmethod
+from trasfomation import to_kepler, to_polar
+import time
+FPS = 30
+
+YELLOW = (255, 200, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
-def animation(x, y, coordinates, dt):
-    pygame.init()
-    display_size = (1200, 800)
-    screen = pygame.display.set_mode(display_size)
-    matrix = np.array([[0, -0.5], [-1, -0.3], [1, 1]])
-    finished = False
-    dx = 0
-    dy = 0
-    r = 120
-    dr = 30
-    clock = pygame.time.Clock()
-    plt.xlim(min(x) * 1.2, max(x) * 1.2)
-    plt.ylim(min(y) * 1.2, max(y) * 1.2)
-    plt.xlabel("time, sec")
-    plt.ylabel("x, m")
-    plt.grid()
-    plot_surf = pygame.image.load("plot.png")
-    while not finished:
-        if r % 120 == 0:
-            plt.plot(x[r - 4 * dr:r], y[r - 4 * dr:r], 'r')
-            plt.savefig("plot.png")
+class Window:
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    def create_text(self, text, color, position, size, screen):
+        f1 = pygame.font.Font(None, size)
+        text1 = f1.render(text, True,
+                          color, BLACK)
+        screen.blit(text1, position)
+
+
+class Menu(Window):
+
+    def __init__(self, screen):
+        self.buttons = []
+        self.positions = []
+        self.screen = screen
+        self.create_text("Launch your satellite!", WHITE, (250, 50), 100, self.screen)
+        self.create_text("Start", WHITE, (250, 250), 80, self.screen)
+        self.create_text("Parameters", WHITE, (250, 400), 80, self.screen)
+        self.create_text("Exit", WHITE, (250, 550), 80, self.screen)
+        self.colors = [WHITE, WHITE, WHITE, WHITE]
+
+    def run(self):
+        finished = False
+        clock = pygame.time.Clock()
+        while not finished:
             clock.tick(FPS)
-            plot_surf = pygame.image.load("plot.png")
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+                    return 0
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if 250 < mouse_pos[0] < 400 and 250 < mouse_pos[1] < 300:
+                        return 1
+                    if 250 < mouse_pos[0] < 400 and 401 < mouse_pos[1] < 450:
+                        return 2
+                    if 250 < mouse_pos[0] < 400 and 550 < mouse_pos[1] < 600:
+                        return 3
 
-        plot_rect = plot_surf.get_rect(
-            bottomright=(625, 475))
-        screen.blit(plot_surf, plot_rect)
+            mouse_pos = pygame.mouse.get_pos()
+            if 250 < mouse_pos[0] < 400 and 250 < mouse_pos[1] < 300:
+                self.colors[1] = YELLOW
+            else:
+                self.colors[1] = WHITE
+            if 250 < mouse_pos[0] < 400 and 401 < mouse_pos[1] < 450:
+                self.colors[2] = YELLOW
+            else:
+                self.colors[2] = WHITE
+            if 250 < mouse_pos[0] < 400 and 550 < mouse_pos[1] < 600:
+                self.colors[3] = YELLOW
+            else:
+                self.colors[3] = WHITE
+            self.draw_objects()
+            pygame.display.update()
+            self.screen.fill(BLACK)
 
-        new_co = coordinates[r] @ matrix
-        pygame.draw.polygon(screen, [0, 0, 0], ([display_size[0] / 2, 0], [display_size[0], 0],
-                                                [display_size[0], display_size[1]], [display_size[0] / 2, display_size[1]]))
-        pygame.draw.circle(screen, [255, 255, 255], [display_size[0]*0.75 + dx, display_size[1] / 2 + dy], 20)
-        pygame.draw.circle(screen, [255, 255, 255], [display_size[0]*0.75 + new_co[0] + dx,
-                                                     display_size[1] / 2 + new_co[1] + dy], 10)
-        # time.sleep(dt)
-        pygame.display.update()
-        screen.fill([255, 255, 255])
-        r += dr
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    def draw_objects(self):
+        self.create_text("Launch your satellite!", self.colors[0], (250, 50), 100, self.screen)
+        self.create_text("Start", self.colors[1], (250, 250), 80, self.screen)
+        self.create_text("Parameters", self.colors[2], (250, 400), 80, self.screen)
+        self.create_text("Exit", self.colors[3], (250, 550), 80, self.screen)
+
+
+class Animation(Window):
+
+    def __init__(self, x, y, coordinates, screen, delta_t):
+        self.matrix = np.array([[0, -0.5], [-1, -0.3], [1, 1]])
+        self.finished = False
+        self.dt = delta_t
+        self.dx = 0
+        self.dy = 0
+        self.counter = 0
+        self.plot_step = 10
+        self.acceleration = 10
+        self.x = x
+        self.y = y
+        self.coordinates = coordinates
+        self.screen = screen
+
+    def run(self):
+        finished = False
+        clock = pygame.time.Clock()
+        while not finished:
+            clock.tick(FPS)
+            # if self.counter % self.plot_step == 0:
+            #     self.plot()
+            self.draw_objects()
+            #time.sleep(self.dt)
+            self.counter += self.acceleration
+            pygame.display.update()
+            self.screen.fill(WHITE)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_DOWN]:
+                self.dy += 2
+            if keys[pygame.K_UP]:
+                self.dy -= 2
+            if keys[pygame.K_RIGHT]:
+                self.dx += 2
+            if keys[pygame.K_LEFT]:
+                self.dx -= 2
+
+            if self.counter > len(self.coordinates) - 1:
                 finished = True
 
-        keys = pygame.key.get_pressed()
+    def plot(self):
+        plt.cla()
+        plt.xlim(min(self.x) * 1.2, max(self.x) * 1.2)
+        plt.ylim(min(self.y) * 1.2, max(self.y) * 1.2)
+        plt.xlabel("time, sec")
+        plt.ylabel("x, m")
+        plt.grid()
+        plt.plot(self.x[0:self.counter], self.y[0:self.counter], 'r')
+        plt.savefig("plot.png")
 
-        if keys[pygame.K_DOWN]:
-            dy += 2
-        if keys[pygame.K_UP]:
-            dy -= 2
-        if keys[pygame.K_RIGHT]:
-            dx += 2
-        if keys[pygame.K_LEFT]:
-            dx -= 2
+    def draw_objects(self):
+        # plot_surf = pygame.image.load("plot.png")
+        # plot_rect = plot_surf.get_rect(
+        #     bottomright=(625, 475))
+        # self.screen.blit(plot_surf, plot_rect)
+        new_co = self.coordinates[self.counter] @ self.matrix
+        pygame.draw.polygon(self.screen, [0, 0, 0], ([self.screen.get_width() / 2, 0], [self.screen.get_width(), 0],
+                                                         [self.screen.get_width(), self.screen.get_height()],
+                                                         [self.screen.get_width() / 2, self.screen.get_height()]))
+        pygame.draw.circle(self.screen, [255, 255, 255], [self.screen.get_width() * 0.75 + self.dx,
+                                                          self.screen.get_height() / 2 + self.dy], 20)
+        pygame.draw.circle(self.screen, [255, 255, 255], [self.screen.get_width() * 0.75 + new_co[0] + self.dx,
+                                                          self.screen.get_height() / 2 + new_co[1] + self.dy], 10)
 
-        if r > len(coordinates) - 1:
-            finished = True
 
-    pygame.quit()
+class LoadingWindow(Window):
+
+    def __init__(self, screen, integrator):
+        self.screen = screen
+        self.mas_x = np.array([])
+        self.mas_vx = np.array([])
+        self.mas_vy = np.array([])
+        self.mas_vz = np.array([])
+        self.clock = 0
+        self.mas_t = np.array([0])
+        self.mas_dt = np.array([])
+        self.integrator = integrator
+        self.q = np.array([6400000, 0, 0, 0, 6000, 0])
+        self.position = np.array([[self.q[0], self.q[1], self.q[2]]], dtype=object)
+
+    def run(self):
+        finished = False
+        clock = pygame.time.Clock()
+        while not finished:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return 0
+
+            pos = np.array([[self.q[0] / 25000, self.q[1] / 25000, self.q[2] / 25000]])
+            self.position = np.append(self.position, pos, axis=0)
+            # print(pos)
+            self.mas_x = np.append(self.mas_x, self.q[0])
+            self.mas_vx = np.append(self.mas_vx, self.q[3])
+            self.mas_vy = np.append(self.mas_vx, self.q[4])
+            self.mas_vz = np.append(self.mas_vx, self.q[5])
+            self.clock += self.integrator.dt
+            self.mas_t = np.append(self.mas_t, self.clock)
+            self.mas_dt = np.append(self.mas_dt, self.integrator.dt)
+            self.q = self.integrator.calc_next_step(self.q, 0)
+            if max(self.mas_t) > 10000:
+                return 1
+
+            self.create_text("Loading " + str(round(max(self.mas_t) / 10000 * 100)) + "%", WHITE, (350, 350),
+                             100, self.screen)
+            pygame.display.update()
+            self.screen.fill(BLACK)
 
 
 def animation_map(q, dt):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from forces import ResistForce, GravityForce, SunForce, TestForce
 from IntegrateMethods import RK4Method, EulerMethod1, EulerMethod2, DormandPrinceMethod
 from astropy.time import Time
 import numpy as np
+import pygame
 import graphics
 
 
@@ -10,7 +11,6 @@ square = 0.2
 cx = 1
 time = Time('1999-01-01T00:00:00.123456789', format='isot', scale='utc')
 
-q = [np.array([6700000, 0, 1000000, 0, 6000, 4000])]
 
 air_force = ResistForce(square, cx)
 g_force = GravityForce(mass)
@@ -19,8 +19,8 @@ test_force = TestForce()
 
 # testing
 
-delta_t = 0.1
-Forces = [g_force, air_force]
+delta_t = 10
+Forces = [g_force]
 integrator1 = RK4Method(delta_t, mass, Forces)
 integrator2 = DormandPrinceMethod(delta_t, mass, Forces)
 integrator3 = EulerMethod1(delta_t, mass, Forces)
@@ -28,12 +28,14 @@ integrator4 = EulerMethod2(delta_t, mass, Forces)
 
 integrator = integrator1   # you can change the method here
 
-clock = 0
-mas_t = np.array([0])
-
-while mas_t[-1] < 1000:
-    q.append(integrator.calc_next_step(q[-1], time))
-    mas_t = np.append(mas_t, clock)
-    clock += integrator.dt
-
-graphics.animation_map(q, 100)
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+menu = graphics.Menu(screen)
+choice = menu.run()
+if choice == 1:
+    load = graphics.LoadingWindow(screen, integrator)
+    a = load.run()
+    if a:
+        anim = graphics.Animation(load.mas_t, load.mas_x, load.position, screen, delta_t)
+        anim.run()
+pygame.quit()
