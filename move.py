e2f832c900afbf41eeccd209e180791ab8aee83b
Diana2410-start <yakusheva2410@yandex.ru>
2021-05-15 12:56:47 +0300

    move

---
+++ move.py
@@ -0,0 +1,75 @@
+import pygame
+import random
+
+WIDTH = 800
+HEIGHT = 650
+FPS = 30
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("MilitaryRocketWithFlames.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+
+    # Рендеринг
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
+
+
+
+#window = pg.display.set_mode((800, 600))
+#screen = pg.Surface((800, 600))
+#cube = pg.image.load("cube.jpg")
+#start_game = True
+#x_cube = 0
+
+#while start_game == True:
+#    x_cube += 0.1
+#    window.blit(screen, (0, 0))
+#    screen.blit(cube, (x_cube, 100))
+#    pg.display.update()
+
+pg.quit()
